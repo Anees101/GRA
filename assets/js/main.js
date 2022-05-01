4228ab813767f9e9aabfe3d119ce3086b8400f20
Crafty Code <vertualid101@gmail.com>
2022-05-01 17:35:25 +0530

    a
    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,36 @@
+
+//Pages
+//--------------------------------------------------
 fetch("./assets/pages/navbar.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
-    document.querySelector("asdd").innerHTML = data;
+    document.querySelector("header").innerHTML = data;
   });
-
-fetch("./footer.html")
+//--------------------------------------------------
+fetch("./assets/pages/footer.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+//--------------------------------------------------
+fetch("./assets/pages/script.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("jsa").innerHTML = data;
+  });
+//--------------------------------------------------
+fetch("./assets/pages/search.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("search").innerHTML = data;
+  });
+//--------------------------------------------------
+// Pages
